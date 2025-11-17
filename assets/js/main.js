8d35c8becaf27526d3a7045fceb31fd3c9fc60f2
Mohamed <sharawym117@gmail.com>
2025-11-18 01:03:21 +0200

    Push Githup

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*=============== SHOW MENU ===============*/
+let open_nav = document.querySelector(".open_nav");
+let list = document.querySelector(".list");
+let close_nav = document.querySelector(".close_nav");
+
+open_nav.addEventListener("click" , ()=>{
+    list.classList.add("active");
+})
+
+
+close_nav.addEventListener("click" , ()=>{
+    list.classList.remove("active");
+})
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
